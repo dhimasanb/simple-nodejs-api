4740fb9d5b8fe9d64525b19d86acf72a7a7d46c0
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-06-16 13:20:29 +0700

    Add config mongodb

---
+++ app/config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url : mongodb://<root>:<root>@ds127982.mlab.com:27982/goangkot
+};
--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const app            = express();
+const db             = require('./config/db');
 
 const port = 1996;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require('./app/routes')(app, {});
-app.listen(port, () => {
-  console.log('We are live on ' + port);
-});
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+  require('./app/routes')(app, database);
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });               
+})
