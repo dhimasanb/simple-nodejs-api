c7657a9394f80693bb4673b46449446aca03bb8f
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-06-17 10:30:49 +0700

    Add route note post

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,7 +1,15 @@
 module.exports = function(app, db) {
     app.post('/notes', (req, res) => {
-    // You'll create your note here.
-    console.log(req.body)
-    res.send('Hello')
+    const note = { 
+        text: req.body.body,
+        title: req.body.title
+    };
+    db.collection('notes').insert(note, (err, result) => {
+      if (err) { 
+        res.send({ 'error': 'An error has occurred' }); 
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
   });
 };
